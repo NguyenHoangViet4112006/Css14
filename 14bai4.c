452f237e11a84183abace52a88bc6010f3514ae1
NguyenHoangViet4112006 <viet4112006@gmail.com>
2025-05-29 10:59:25 +0700

    14bai4.c

--- 14bai4.c
+++ 14bai4.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    char kytu[] = "darkbruhlmao";
-    char real;
+    char kytu[] = "laptrinhc";
+    char check;
     int count = 0;
     printf("nhap vao mot ky tu bat ky: ");
-    scanf("%c", &real);
+    scanf("%c", &check);
     for (int i = 0; kytu[i] != '\0'; i++){
-        if (kytu[i] == real){
+        if (kytu[i] == check){
             count++;
         }
     }
-    printf("ky tu %c xuat hien %d lan", real, count);
+    printf("ky tu %c xuat hien %d lan", check, count);
 
 
 
